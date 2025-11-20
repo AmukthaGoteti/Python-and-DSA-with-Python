306ed2d31e80ce37ca49aa8f00d845a8313f59a9
Amuktha Goteti <amukthagoteti55@gmail.com>
2025-11-20 16:31:04 +0530

    commit

--- python-and-dsa/Week-1/Python_Operators.py
+++ python-and-dsa/Week-1/Python_Operators.py
@@ -58,3 +58,15 @@ b = 20
 c = a
 print(a is not b)
 print(a is c)
+# Membership Operators
+x = 24
+y = 20
+list = [10, 20, 30, 40 , 50]
+if (x not in list):
+    print("x is NOT prsent in given list")
+else:
+    print("x is present in given list")
+if (y in list):
+    print("y is present in given list")
+else:
+    print("y is NOT present in given list")
