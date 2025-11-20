baac0ca5942a0693c1034a0efd758c1238d46d5a
Amuktha Goteti <amukthagoteti55@gmail.com>
2025-11-20 17:21:05 +0530

    commit

---
+++ python-and-dsa/Week-1/If_else.py
@@ -0,0 +1,6 @@
+# if Statement
+num = int(input("Enter a number: "))
+# check if the number is greater than 0
+if num > 0:
+    print(f'{num} is a positive number.')
+print('A statement outside the if block.')
