96e78eefcd3c8a64f3a5c24b89899f02e6342ced
Amuktha Goteti <amukthagoteti55@gmail.com>
2025-11-20 17:26:24 +0530

    commit

--- python-and-dsa/Week-1/If_else.py
+++ python-and-dsa/Week-1/If_else.py
@@ -4,3 +4,12 @@ num = int(input("Enter a number: "))
 if num > 0:
     print(f'{num} is a positive number.')
 print('A statement outside the if block.')
+# indentation
+x = 1
+total = 0
+# start of the if statement
+if x!= 0:
+    total += x
+    print(total)
+# end of the if statement
+print("This is always executed.")
