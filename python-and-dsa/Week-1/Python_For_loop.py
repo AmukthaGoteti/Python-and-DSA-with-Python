924f40af5e9934f3e64103a656d54eb467ab5f47
Amuktha Goteti <amukthagoteti55@gmail.com>
2025-11-28 13:59:07 +0530

    commit

--- python-and-dsa/Week-1/Python_For_loop.py
+++ python-and-dsa/Week-1/Python_For_loop.py
@@ -2,3 +2,10 @@ languages = ['Swift', 'Python', 'Go']
 # Access elements of the list one by one
 for lang in languages:
     print(lang)
+Langauges = ['Swift', 'Python', 'Go']
+# start of the loop
+for lang in languages:
+    print(lang)
+    print('-----')
+# end of the for loop
+print('Last statement')
