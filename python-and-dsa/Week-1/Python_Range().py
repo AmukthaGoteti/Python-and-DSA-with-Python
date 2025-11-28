2d15eed3c4f702294d88c029cf890d254fa61241
Amuktha Goteti <amukthagoteti55@gmail.com>
2025-11-28 12:51:10 +0530

    commit

--- python-and-dsa/Week-1/Python_Range().py
+++ python-and-dsa/Week-1/Python_Range().py
@@ -24,3 +24,10 @@ print()
 for i in range(25, 2, -2):
     print(i, end=" ")
 print()
+from itertools import chain
+# Using chain method
+print("Concatenating the result")
+res = chain(range(5), range(10, 20, 2))
+for i in res:
+    print(i, end=" ")
+print()
