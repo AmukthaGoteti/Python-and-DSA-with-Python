cdbeab97088106160c48275b1e097ac2e8e4cd82
Amuktha Goteti <amukthagoteti55@gmail.com>
2025-12-04 12:00:49 +0530

    commit

--- python-and-dsa/Week-1/Python_While.py
+++ python-and-dsa/Week-1/Python_While.py
@@ -2,3 +2,10 @@ number = 1
 while number <=3:
     print(number)
     number = number + 1
+# Print numbers until the user enters 0
+number = int(input("Enter a number: "))
+# Iterate until the user enters 0
+while number != 0:
+    print(f'You entered {number}')
+    number = int(input("Enter a number: "))
+print('The End')
