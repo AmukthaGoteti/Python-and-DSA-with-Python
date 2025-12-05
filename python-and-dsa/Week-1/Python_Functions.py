a0e36d1346f559d062d44eaedf05a13fc55bebe2
Amuktha Goteti <amukthagoteti55@gmail.com>
2025-12-05 11:24:17 +0530

    commit

--- python-and-dsa/Week-1/Python_Functions.py
+++ python-and-dsa/Week-1/Python_Functions.py
@@ -9,3 +9,9 @@ def greet(name):
 # Pass Argument
 greet("Jhon")
 greet("David")
+# Function with Two Arguments
+def add_numbers(num1, num2):
+    sum = num1 + num2
+    print("Sum:", sum)
+# function call with two values
+add_numbers(5, 4)
