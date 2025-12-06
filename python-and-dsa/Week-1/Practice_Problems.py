822d14a9007a71eb09817d02e8a7c5411fdf6336
Amuktha Goteti <amukthagoteti55@gmail.com>
2025-12-06 13:05:35 +0530

    commit

--- python-and-dsa/Week-1/Practice_Problems.py
+++ python-and-dsa/Week-1/Practice_Problems.py
@@ -3,3 +3,13 @@
 print("Hello, World!") # Prints Hello, World!
 print("""Hello
 world""") # Prints Hello in first line and world in second line
+# Practice Problem 2
+# Learn to comment
+class solution:
+    def print_values(self, a, b, c):
+        print(a, end=" ")
+        # print(b, end=" ") # This line is commented out
+        print(c, end=" \n")
+solution = solution()
+a, b, c = input().split()
+solution.print_values(a, b, c)
