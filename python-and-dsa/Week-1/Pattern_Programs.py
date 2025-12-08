8814f46130a011b790cee4c00329ceb913e01f81
Amuktha Goteti <amukthagoteti55@gmail.com>
2025-12-08 15:41:04 +0530

    commit

--- python-and-dsa/Week-1/Pattern_Programs.py
+++ python-and-dsa/Week-1/Pattern_Programs.py
@@ -75,3 +75,15 @@ def inverted_full_pyramid(n):
 n = int(input("Enter the number of rows for the inverted pyramid: "))
 # Print the inverted pyramid pattern
 inverted_full_pyramid(n)
+def hallow_pyramid(n):
+    for i in range(1, n + 1):
+        for j in range(1, 2 * n):
+            if j == n - i + 1 or j == n + i - 1 or i == n:
+                print("*", end="")
+            else:
+                print(" ", end="")
+        print()
+# Set the number of rows for the hollow pyramid
+n = int(input("Enter the number of rows for the hollow pyramid: "))
+# Print the hollow pyramid pattern
+hallow_pyramid(n)
