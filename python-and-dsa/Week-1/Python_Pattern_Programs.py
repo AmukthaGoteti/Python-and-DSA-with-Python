44c0804aa4b77ba92c35452f37dc08faba059fa4
Amuktha Goteti <amukthagoteti55@gmail.com>
2025-12-09 12:42:32 +0530

    commit

--- python-and-dsa/Week-1/Python_Pattern_Programs.py
+++ python-and-dsa/Week-1/Python_Pattern_Programs.py
@@ -162,3 +162,18 @@ def inverted_half_pyramid(n):
 # Example usage
 num_rows = int(input("Enter the number of rows for the inverted half pyramid: "))
 inverted_half_pyramid(num_rows)
+# Hallow Inverted half Pyramid Pattern in Python
+def print_hollow_inverted_half_pyramid(rows):
+    for i in range(rows, 0, -1):
+        for j in range(rows - i):
+            print(" ", end="")
+        for j in range(i):
+            if j == 0 or j == i - 1 or i == rows:
+                print("*", end="")
+            else:
+                print(" ", end="")
+        print()
+# Example usage
+num_rows = 5
+print("Hollow Inverted Half Pyramid:")
+print_hollow_inverted_half_pyramid(num_rows)
