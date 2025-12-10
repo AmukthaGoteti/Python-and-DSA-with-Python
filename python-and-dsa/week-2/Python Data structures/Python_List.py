bee3e768ada2ff4ae3342d6f17a133471d164923
Amuktha Goteti <amukthagoteti55@gmail.com>
2025-12-10 19:43:08 +0530

    commit

--- python-and-dsa/week-2/Python Data structures/Python_List.py
+++ python-and-dsa/week-2/Python Data structures/Python_List.py
@@ -3,3 +3,10 @@
 # A list of 3 elements
 ages = [19, 29, 29]
 print(ages)
+# List Items of Different Types
+# A list containing strings, numbers and another list
+student = ['Jack', 32, 'Computer Science', [2, 4]]
+print(student)
+# An empty List
+empty_list = []
+print(empty_list)
