40bf6e1cd5cf16a52625745dc429588eac05e157
Amuktha Goteti <amukthagoteti55@gmail.com>
2025-12-12 16:34:20 +0530

    commit

--- python-and-dsa/Week-2/Python_Data_Structures/Python_Tuple.py
+++ python-and-dsa/Week-2/Python_Data_Structures/Python_Tuple.py
@@ -8,3 +8,8 @@ languages = ('Python', 'Swift', 'C++')
 print("Languages[0]: ", languages[0])
 # Access the 3rd element
 print("Languages[2]: ", languages[2])
+# Tuple Cannot be Modified
+# cars = ('BMW', 'Tesla', 'Ford', 'Toyota')
+# trying to modify a tuple
+# cars[0] = 'Nissan' -> Error
+# print(cars)
