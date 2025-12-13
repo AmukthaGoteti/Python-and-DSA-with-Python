5f552bd9707d7a656c45aacca583a724694b9917
Amuktha Goteti <amukthagoteti55@gmail.com>
2025-12-13 12:26:51 +0530

    Creating a set in Python

---
+++ python-and-dsa/Week-2/Python_Data_Structures/Python_Sets.py
@@ -0,0 +1,11 @@
+# Python Sets
+# Creating a set in Python
+# Create a set of Integer Type
+student_id = {112, 114, 116, 118, 115}
+print('Student ID: ', student_id)
+# Create a set of String Type
+vowel_letters = {'a', 'e', 'i', 'o', 'u'}
+print('Vowel Letters: ', vowel_letters)
+# Create a set of Mixed Type
+mixed_set = {'Hello', 101, -2, 'Bye'}
+print('Set of mixed data types: ', mixed_set)
