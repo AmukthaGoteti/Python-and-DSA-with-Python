36b16a836cecec623c3c3699b18dcbb57aba68eb
Amuktha Goteti <amukthagoteti55@gmail.com>
2025-12-16 13:46:53 +0530

    Finding Number of Set elements

--- python-and-dsa/Week-2/Python_Data_Structures/Python_Sets.py
+++ python-and-dsa/Week-2/Python_Data_Structures/Python_Sets.py
@@ -58,3 +58,8 @@ fruits = {"Apple", "Peach", "Mango"}
 # for loop to access each fruits
 for fruit in fruits:
     print(fruit)
+# Finding Number of Set elements
+even_numbers = {2, 4, 6, 8}
+print("Set: ", even_numbers)
+# Find number of elements
+print("Total Elements: ", len(even_numbers))
