09801476a6e8f12fadb80945eecd628d4fb682e4
Amuktha Goteti <amukthagoteti55@gmail.com>
2025-12-17 13:12:34 +0530

    Iterate Through a Dictionary

--- python-and-dsa/Week-2/Python_Data_Structures/Python_Dictionary.py
+++ python-and-dsa/Week-2/Python_Data_Structures/Python_Dictionary.py
@@ -48,3 +48,16 @@ country_capitals = {
 # change the value of "Italy" key to "Rome"
 country_capitals["Italy"] = "Rome"
 print(country_capitals)
+# Iterate Through a Dictionary
+country_capitals = {
+  "United States": "Washington D.C.", 
+  "Italy": "Rome" 
+}
+# print dictionary keys one by one
+for country in country_capitals:
+    print(country)
+print()
+# print dictionary values one by one
+for country in country_capitals:
+    capital = country_capitals[country]
+    print(capital)
