b819ba08a7208002f8043266c44ac4c0450274e5
Amuktha Goteti <amukthagoteti55@gmail.com>
2025-12-17 13:06:34 +0530

    Change Dictionary Items

--- python-and-dsa/Week-2/Python_Data_Structures/Python_Dictionary.py
+++ python-and-dsa/Week-2/Python_Data_Structures/Python_Dictionary.py
@@ -39,3 +39,12 @@ country_capitals = {
 # Clear the dictionary
 country_capitals.clear()
 print(country_capitals)
+# Change Dictionary Items
+country_capitals = {
+  "Germany": "Berlin", 
+  "Italy": "Naples", 
+  "England": "London"
+}
+# change the value of "Italy" key to "Rome"
+country_capitals["Italy"] = "Rome"
+print(country_capitals)
