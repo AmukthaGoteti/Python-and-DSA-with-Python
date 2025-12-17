729176804ff8f4f2a6cfc6fdec245bbee76df01c
Amuktha Goteti <amukthagoteti55@gmail.com>
2025-12-17 13:45:33 +0530

    Dictionary Membership Test

--- python-and-dsa/Week-2/Python_Data_Structures/Python_Dictionary.py
+++ python-and-dsa/Week-2/Python_Data_Structures/Python_Dictionary.py
@@ -82,3 +82,13 @@ print(len(countries))          # Output: 0
     popitem()	    Returns the last inserted key and value as a tuple.
     copy()	        Returns a copy of the dictionary.
 """
+# Dictionary Membership Test
+file_types = {
+    ".txt": "Text File",
+    ".pdf": "PDF Document",
+    ".jpg": "JPEG Image",
+}
+# use of in and not in operators
+print(".pdf" in file_types)       # Output: True
+print(".mp3" in file_types)       # Output: False
+print(".mp3" not in file_types)   # Output: True
