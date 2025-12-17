e07a073f11edf5b984d512260de5984d8b1bc7be
Amuktha Goteti <amukthagoteti55@gmail.com>
2025-12-17 11:54:32 +0530

    Creating a dictionary

---
+++ python-and-dsa/Week-2/Python_Data_Structures/Python_Dictionary.py
@@ -0,0 +1,9 @@
+# Python Dictionary
+# Creating a dictionary
+country_capitals = {
+    "Germany" : "Berlin",
+    "Canada" : "Ottawa",
+    "England" : "London"
+}
+# Printing the dictionary
+print(country_capitals)
