4c66b408557642ff6eb91a5394c8e67dba9c4c04
Amuktha Goteti <amukthagoteti55@gmail.com>
2025-12-18 13:17:11 +0530

    Create Strings using single and double quotes

---
+++ python-and-dsa/Week-2/Python_Data_Structures/Python_Strings.py
@@ -0,0 +1,7 @@
+# Python Strings
+# Create a string using double quotes
+string1 = "Python Programming"
+print(string1)
+# Create a string using single quotes
+string1 = 'Python Programming'
+print(string1)
