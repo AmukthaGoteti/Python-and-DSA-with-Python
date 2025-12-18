5a6d600d58c5c008420bba52b570703f1f71f8c9
Amuktha Goteti <amukthagoteti55@gmail.com>
2025-12-18 13:21:04 +0530

    Create string type variable

--- python-and-dsa/Week-2/Python_Data_Structures/Python_Strings.py
+++ python-and-dsa/Week-2/Python_Data_Structures/Python_Strings.py
@@ -5,3 +5,8 @@ print(string1)
 # Create a string using single quotes
 string1 = 'Python Programming'
 print(string1)
+# Create string type variable
+name = "Python"
+print(name)
+message = "I love Python"
+print(message)
