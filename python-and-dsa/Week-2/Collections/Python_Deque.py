104dc4ba77b12e269232998014f1ce09641e6e89
Amuktha Goteti <amukthagoteti55@gmail.com>
2025-12-19 07:53:19 +0530

    Appending and Deleting Deque Items - Adding elements to the right

--- python-and-dsa/Week-2/Collections/Python_Deque.py
+++ python-and-dsa/Week-2/Collections/Python_Deque.py
@@ -3,3 +3,8 @@ from collections import deque
 # Declaring deque
 de = deque(['name', 'age', 'DOB'])
 print(de)
+# Appending and Deleting Deque Items
+de = deque([10, 20, 30])
+# Add elements to the right
+de.append(40)
+print(de)
