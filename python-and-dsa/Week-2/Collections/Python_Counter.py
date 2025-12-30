2ce4b5515b591a4b316a2c00c141b957641bbe8e
Amuktha Goteti <amukthagoteti55@gmail.com>
2025-12-30 13:26:28 +0530

    commit

--- python-and-dsa/Week-2/Collections/Python_Counter.py
+++ python-and-dsa/Week-2/Collections/Python_Counter.py
@@ -28,3 +28,32 @@ x_keys = list(x.keys())
 x_values = list(x.values())
 print(x_keys)
 print(x_values)
+# Elements on variety of counter objects with
+# different data-containers
+# import counter class from collections module
+from collections import Counter
+# Creation of a Counter Class Object using
+# a string as an iterable data container
+# Example - 1
+a = Counter('GeeksforGeeks')
+# Elements of Counter object
+for i in a.elements():
+    print(i, end=" ")
+print()
+# Example - 2
+b = Counter({'geeks' : 4, 'for': 1, 'gfg' : 2,
+             'python' : 3})
+for i in b.elements():
+    print(i, end=" ")
+print()
+# Example - 3
+c = Counter([1, 2, 21, 12, 2, 44, 5, 13, 15, 5, 19,
+             21, 5])
+for i in c.elements():
+    print(i, end =" ")
+print()
+# Example - 4
+d = Counter(a = 2, b = 3, c = 6, d = 1, e = 5)
+for i in d.elements():
+    print(i, end =" ")
+print()
