7c09e4abc42f37ea30968ed639e57424f29fe823
Amuktha Goteti <amukthagoteti55@gmail.com>
2025-12-30 12:57:44 +0530

    commit

--- python-and-dsa/Week-2/Collections/Python_Counter.py
+++ python-and-dsa/Week-2/Collections/Python_Counter.py
@@ -9,3 +9,22 @@ x = Counter("geeksforgeeks")
 for i in x.elements():
     print(i, end= " ")
 print()
+# We can also create Counter class object sing a list
+# as an iterable data container
+# import counter class from collection module
+from collections import Counter
+# Creating a counter class object usng list as an
+# iterable data container
+a = [12, 3, 4, 3, 5, 11, 12, 6, 7]
+x = Counter(a)
+# directly printing whole x
+print(x)
+# We can also use .key() and .values() methods to
+# access Counter class object
+for i in x.keys():
+    print(i, ":", x[i])
+# We can also make  list of keys and values of x
+x_keys = list(x.keys())
+x_values = list(x.values())
+print(x_keys)
+print(x_values)
