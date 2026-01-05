34bbd443f0a9bee4de6155197fe53feeae4e2919
Amuktha Goteti <amukthagoteti55@gmail.com>
2026-01-05 21:11:13 +0530

    commit

--- python-and-dsa/Week-2/Collections/Python_Ordered_Dict.py
+++ python-and-dsa/Week-2/Collections/Python_Ordered_Dict.py
@@ -4,6 +4,18 @@
     built-in dictionary dict 
     that remembers the order in which keys are inserted.
 """
+"""
+    It provides extra powerful features, such as:
+        1. Reordering keys dynamically with 
+        move_to_end() (useful for FIFO/LIFO access).
+        2. Popping items from 
+        either end with popitem(last=True/False).
+        3. Order-sensitive equality checks 
+        (two OrderedDicts with same items 
+        but different order are not equal).
+        4. Easy implementation of data structures
+        like queues, stacks, or LRU caches.
+"""
 from collections import OrderedDict
 od = OrderedDict()
 od['apple'] = 1
