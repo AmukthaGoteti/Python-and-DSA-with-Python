c59690a70ea03bef7df46c800f3f66b1b723c85d
Amuktha Goteti <amukthagoteti55@gmail.com>
2026-01-06 12:39:54 +0530

    commit

--- python-and-dsa/Week-2/Collections/Python_Ordered_Dict.py
+++ python-and-dsa/Week-2/Collections/Python_Ordered_Dict.py
@@ -73,3 +73,13 @@ for k, v in od.items():
 od['c'] = 5
 for k, v in od.items():
     print(k, v)
+# Equality checks consider order
+"""
+    Unlike regular dicts, orderedDic checks both content
+    and order for equality, so differing orders make them
+    unequal.
+"""
+from collections import OrderedDict
+od1 = OrderedDict([('a', 1), ('b', 2), ('c', 3)])
+od2 = OrderedDict([('c', 3), ('b', 2), ('a', 1)])
+print(od1 == od2)
