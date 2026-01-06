7f747a70bb359965da6ab816620cfc0e17b83b52
Amuktha Goteti <amukthagoteti55@gmail.com>
2026-01-06 11:52:21 +0530

    commit

--- python-and-dsa/Week-2/Collections/Python_Ordered_Dict.py
+++ python-and-dsa/Week-2/Collections/Python_Ordered_Dict.py
@@ -22,3 +22,26 @@ od['apple'] = 1
 od['banana'] = 2
 od['cherry'] = 3
 print(list(od.items()))
+"""
+    Both OrderedDict and normal dict 
+    preserve insertion order
+    But only OrderedDict gives 
+    advanced control over order
+"""
+from collections import OrderedDict
+print("dict")
+d={}
+d['a'] = 1
+d['b'] = 2
+d['c'] = 3
+d['d'] = 4
+for key, val in d.items():
+    print(key, val)
+print('ordered dict')
+od = OrderedDict()
+od['d'] = 4
+od['b'] = 2
+od['a'] = 1
+od['c'] = 3
+for key, val in od.items():
+    print(key, val)
