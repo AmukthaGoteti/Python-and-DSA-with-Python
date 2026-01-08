d22b090e753886afc9b48a8fac6ae01b89400fd1
Amuktha Goteti <amukthagoteti55@gmail.com>
2026-01-08 20:11:44 +0530

    defaultdict

--- python-and-dsa/Week-2/Collections/Python_Default_Dict.py
+++ python-and-dsa/Week-2/Collections/Python_Default_Dict.py
@@ -24,3 +24,11 @@ print(d['juices'])
     gives a default value when a key is missing, 
     instead of showing an error.
 """
+# UUse Case of defaultdict
+# Using List as Default Factory
+from collections import defaultdict
+d = defaultdict(list)
+for i in range(5):
+    d[i].append(i)
+print("Dictionary with values as list: ")
+print(d)
