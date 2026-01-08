182f9ac466f13b3d235e98ec904689d5c79281f1
Amuktha Goteti <amukthagoteti55@gmail.com>
2026-01-08 17:47:36 +0530

    commit

--- python-and-dsa/Week-2/Collections/Python_Default_Dict.py
+++ python-and-dsa/Week-2/Collections/Python_Default_Dict.py
@@ -12,3 +12,15 @@ d['fruits'].append('apple')
 d['vegetables'].append('carrot')
 print(d)
 print(d['juices'])
+# Syntax -> defaultdict(default_Factory)
+"""
+    default_factory: A callable (like int, list, set, str or 
+    a custom function) that provides the default value for 
+    missing keys.
+    If this argument is None, 
+    accessing a missing key raises a KeyError.
+
+    Returnn Value: It returns a dictionary that 
+    gives a default value when a key is missing, 
+    instead of showing an error.
+"""
