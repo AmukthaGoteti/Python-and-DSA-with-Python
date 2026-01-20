c5f5e9f8f1e97aebf7aae57f2f788b8685493ecf
Amuktha Goteti <amukthagoteti55@gmail.com>
2026-01-20 13:15:34 +0530

    commit

--- python-and-dsa/Week-2/Collections/Python_Namedtuple.py
+++ python-and-dsa/Week-2/Collections/Python_Namedtuple.py
@@ -101,3 +101,20 @@ di = {'name': "Nikhil", 'age': 19, 'DOB': '1391997'}
 # using _make() to return namedtuple()
 print("The namedtuple instance using _make(): ")
 print(Student._make(li))
+# Conversion Operation Using _asdict()
+"""
+    This function returns the OrderedDict() 
+    as constructed from the mapped values of namedtuple().
+"""
+import collections
+# Declaring namedtuple()
+Student = collections.namedtuple('Student', ['name', 'age', 'DOB'])
+# Adding values
+S = Student('Nandini', '19', '2541997')
+# initializing iterable
+li = ['Manjeet', '19', '411997']
+# initializing dict
+di = {'name': "Nikhil", 'age': 19, 'DOB': '1391997'}
+# using _asdict() to return an OrderedDict()
+print("The OrderedDict instance using namedtuple is  : ")
+print(S._asdict())
