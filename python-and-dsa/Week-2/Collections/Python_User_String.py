b50fb3bdaa9862ea02cd0016511b9c77c87ad411
Amuktha Goteti <amukthagoteti55@gmail.com>
2026-05-26 22:42:17 +0530

    commit

--- python-and-dsa/Week-2/Collections/Python_User_String.py
+++ python-and-dsa/Week-2/Collections/Python_User_String.py
@@ -14,35 +14,3 @@ print("\n", String1, "\n")
 # with double Quotes 
 String1 = "I'm a Geek"
 print(String1, "\n")
-# Python program to demonstrate
-# userstring
-from collections import UserString
-d = 12344
-# Creating an UserDict
-userS = UserString(d)
-print(userS.data)
-# Creating an empty UserDict
-userS = UserString("")
-print(userS.data)
-# Python program to demonstrate
-# userstring
-from collections import UserString
-# Creating a Mutable String
-class Mystring(UserString):
-    # Function to append to
-    # string
-    def append(self, s):
-        self.data += s   
-    # Function to remove from 
-    # string
-    def remove(self, s):
-        self.data = self.data.replace(s, "") 
-# Driver's code
-s1 = Mystring("Geeks")
-print("Original String:", s1.data)
-# Appending to string
-s1.append("s")
-print("String After Appending:", s1.data)
-# Removing from string
-s1.remove("e")
-print("String after Removing:", s1.data)
