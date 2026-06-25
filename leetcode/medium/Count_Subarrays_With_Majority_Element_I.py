3d74806102c96c8b67f39900fc8a5afe4e9de7d2
Amuktha Goteti <amukthagoteti55@gmail.com>
2026-06-25 23:44:25 +0530

    commit

---
+++ leetcode/medium/Count_Subarrays_With_Majority_Element_I.py
@@ -0,0 +1,10 @@
+"""
+    Problem Statement:
+        Given a string text, you want to use the characters of text to 
+        form as many instances of the word "balloon" as possible.
+        You can use each character in text at most once. 
+        Return the maximum number of instances that can be formed.
+    
+    Difficulty: Medium
+    URL: https://leetcode.com/problems/count-subarrays-with-majority-element-i/?envType=daily-question&envId=2026-06-25
+"""
